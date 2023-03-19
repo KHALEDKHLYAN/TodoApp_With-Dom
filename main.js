56a71020a687623e6e91ab37f8d82bed91ba2cd6
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-03-19 19:05:51 +0300

    Javascript updates

---
+++ main.js
@@ -0,0 +1,19 @@
+// variables
+
+const addTask = document.getElementById('add-task');
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task');
+
+// Event Listener for Add Button
+
+addTask.addEventListener('click', function(){
+
+    let task = document.createElement('div');
+    task.classList.add('task');
+
+    let li = document.createElement('li');
+    li.innerText = `$(inputTask.value)`;
+    task.appendChild(li);
+
+    
+})
