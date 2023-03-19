1d3c21a041ad3633a92c386a62ee1172db8df855
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-03-19 19:28:44 +0300

    javascript style

--- main.js
+++ main.js
@@ -15,5 +15,13 @@ addTask.addEventListener('click', function(){
     li.innerText = `$(inputTask.value)`;
     task.appendChild(li);
 
-    
+    let checkButton = document.createElement("button");
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+    let deletButton = document.createElement("button");
+    deletButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    deletButton.classList.add('deleteTask');
+    task.appendChild(deletButton);
 })
